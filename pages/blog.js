abe70c6bc51f248cf89173aaf8987232f08cc00f
matuumdq <salinasmateo@outlook.com>
2022-12-15 15:17:37 -0300

    deploy2

--- pages/blog.js
+++ pages/blog.js
@@ -13,7 +13,7 @@ export default function Blog({posts}) {
         <main className="contenedor">
             <h1 className="heading">Blog</h1>
             <div className={styles.grid}>
-                {posts.map(post => (
+                {posts?.map(post => (
                     <Post
                         key={post.id}
                         post={post.attributes}
